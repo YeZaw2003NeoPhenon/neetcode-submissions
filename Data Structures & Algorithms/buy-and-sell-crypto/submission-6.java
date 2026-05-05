f2297db307d1928ba4ae1599ff953658486ad22d
Ye Zaw Win(Neo) <148946248+YeZaw2003NeoPhenon@users.noreply.github.com>
2026-05-05 19:17:01 +0700

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int minPrice = Integer.MAX_VALUE;
+
+        int maxProfit = 0;
+
+        for(int i = 0; i < prices.length; ++i){
+            minPrice = Math.min(minPrice, prices[i]); // buying price
+
+            int profit = prices[i] - minPrice;
+            if(profit < 0){
+                return 0;
+            }
+            maxProfit = Math.max(maxProfit, profit);
+        }
+        return maxProfit;
+    }
+}
