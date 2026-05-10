d738d06d20e745379e49b86be1e63ebdc1511879
Ye Zaw Win(Neo) <148946248+YeZaw2003NeoPhenon@users.noreply.github.com>
2026-05-10 21:01:05 +0700

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int search(int[] nums, int target) {
+
+     return binarySearch(nums, 0, nums.length - 1, target);   
+    }
+
+    public int binarySearch(int[] nums, int l, int r, int target){
+
+        if(l > r){
+            return -1;
+        }
+
+        int m = l + ( r - l) / 2;
+
+        if(nums[m] == target){
+            return m;
+        }
+        if(nums[m] > target){
+            return binarySearch(nums, l, m - 1, target);
+        }
+        else{
+            return binarySearch(nums, m + 1, r, target);
+        }
+
+    }
+}
